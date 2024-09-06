9482ddc97d52f1ce200a7114e9c92bb92f1eb88b
Anna Sophia Maxen <annasophiamaxen@MacBook-Air-tilhrende-Anna.local>
2024-09-06 11:10:26 +0200

    fredag uge 1

--- AC/Uge 1/eksempler fredag.py
+++ AC/Uge 1/eksempler fredag.py
@@ -11,5 +11,20 @@ d = random.random()
 
 for i in range(7):
     if d < i/6:
-        print(i)
+        print(f"It was a {i}")
         break
+
+my_string = "here is my string it says something about horses"
+
+list_of_words = my_string.split(' ') #splitting at each space, does so as default anyways. You can choose anything as splitter
+
+print(list_of_words)
+
+#finding horses
+
+index = list_of_words.index('horses')
+print(index)
+
+sorted = sorted(list_of_words)
+
+print(sorted[:3])
