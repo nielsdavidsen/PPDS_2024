4084da7679b3ba4a6b01b6ba21249b4518122d05
Niels August <nad0015@hotmail.com>
2024-09-24 09:46:46 +0200

    lille rettelse

--- NIELS/Week_3/Assignment_3/handin3.py
+++ NIELS/Week_3/Assignment_3/handin3.py
@@ -14,7 +14,7 @@ def read_word_file(filename):
         return tuple_list
     
 
-def read_word_file2(filename, filter_re_str):
+def read_word_file2(filename, filter_re_str=''):
     with open(filename, 'r') as file:
         ''' Function that reads a file and returns tuples of the strings in the file along with their position in the file. 
         Takes a filename and a string as arguments. The string is then turned into a reg-ex (regular epxression). 
